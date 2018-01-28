27351e61e6d83272c8ca22c69c82792ebed767f1
Ryan Mortenson <ryan.j.mortenson@gmail.com>
2018-01-27 19:31:26 -0700

    Letimer testing

--- project/main.c
+++ project/main.c
@@ -37,6 +37,7 @@
 #include "bspconfig.h"
 #endif
 
+
 /***********************************************************************************************//**
  * @addtogroup Application
  * @{
@@ -79,6 +80,12 @@ uint8_t boot_to_dfu = 0;
 #include "main.h"
 #include "gpio.h"
 #include "cmu.h"
+#include "letimer.h"
+#include "sleep_mode.h"
+
+
+#include "em_gpio.h"
+#include "em_letimer.h"
 
 //***********************************************************************************
 // defined files
@@ -103,14 +110,123 @@ uint8_t boot_to_dfu = 0;
 //***********************************************************************************
 // main
 //***********************************************************************************
+void LETIMER_setup(void)
 
+{
+  /* Enable necessary clocks */
+
+  CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);
+
+  CMU_ClockEnable(cmuClock_CORELE, true);
+
+  CMU_ClockEnable(cmuClock_LETIMER0, true);
+
+  CMU_ClockEnable(cmuClock_GPIO, true);
+
+
+
+  /* Repetition values must be nonzero so that the outputs
+
+     return switch between idle and active state */
+
+  LETIMER_RepeatSet(LETIMER0, 0, 0x01);
+
+  LETIMER_RepeatSet(LETIMER0, 1, 0x01);
+
+
+
+
+
+                /* Set up PF6 pin */
+
+    // This configuration is valid only if PF6 and PF6 are
+
+    // used for output signal. Change this "LOC30" value to
+
+    // preferred value that can be found from Device datasheet
+
+    // section Alternate Functionality Pinout.
+
+
+
+                LETIMER0->ROUTELOC0 = (LETIMER0->ROUTELOC0
+
+                                                & ~_LETIMER_ROUTELOC0_OUT0LOC_MASK)
+
+                                                | LETIMER_ROUTELOC0_OUT0LOC_LOC30;
+
+                LETIMER0->ROUTEPEN |= LETIMER_ROUTEPEN_OUT0PEN;
+
+
+
+                /* Set up PF7 pin */
+
+                LETIMER0->ROUTELOC0 = (LETIMER0->ROUTELOC0
+
+                                                & ~_LETIMER_ROUTELOC0_OUT1LOC_MASK)
+
+                                                | LETIMER_ROUTELOC0_OUT1LOC_LOC30;
+
+                LETIMER0->ROUTEPEN |= LETIMER_ROUTEPEN_OUT1PEN;
+
+
+
+                // [LETIMER0 I/O setup]$
+
+
+
+  /* Set configurations for LETIMER 0 */
+
+  const LETIMER_Init_TypeDef letimerInit =
+
+  {
+
+  .enable         = true,                   /* Start counting when init completed. */
+
+  .debugRun       = false,                  /* Counter shall not keep running during debug halt. */
+
+  .comp0Top       = true,                   /* Load COMP0 register into CNT when counter underflows. COMP0 is used as TOP */
+
+  .bufTop         = false,                  /* Don't load COMP1 into COMP0 when REP0 reaches 0. */
+
+  .out0Pol        = 0,                      /* Idle value for output 0. */
+
+  .out1Pol        = 0,                      /* Idle value for output 1. */
+
+  .ufoa0          = letimerUFOAPwm,         /* PWM output on output 0 */
+
+  .ufoa1          = letimerUFOAPwm,       /* PWM output on output 1*/
+
+  .repMode        = letimerRepeatFree       /* Count until stopped */
+
+  };
+
+
+
+  /* Initialize LETIMER */
+
+  LETIMER_Init(LETIMER0, &letimerInit);
+
+
+
+  // LETimer is 16 bit long and oscillator frequency is 32768Hz.
+
+  // PWM duty-cycle and frequency can be determined using these compare value setting functions.
+
+  // Shortly the first one determines frequency and second one determines duty-cycle. With this
+
+  // Topvalue the minimum duty-cycle change is 1/500=0.2%.
+
+  LETIMER_CompareSet(LETIMER0, 0, 500);
+
+  LETIMER_CompareSet(LETIMER0, 1, 400);
+
+}
 /**
  * @brief  Main function
  */
 int main(void)
 {
-  int i;
-
   // Initialize device
   initMcu();
 
@@ -121,22 +237,22 @@ int main(void)
   gpio_init();
 
   // Initialize clocks
-  cmu_init();
+  //cmu_init();
 
   // Initialize stack
   gecko_init(&config);
 
+  // Initialize the LETIMER
+  LETIMER_setup();
 
-  while (1) {
-		for (i = 0; i < 500000; i++);
-		GPIO_PinOutClear(LED0_port, LED0_pin);
+  LETIMER_IntEnable(LETIMER0, LETIMER_IF_UF);
 
-		for (i = 0; i < 500000; i++);
-		GPIO_PinOutClear(LED1_port, LED1_pin);
+  /* Enable LETIMER0 interrupt vector in NVIC*/
+  NVIC_EnableIRQ(LETIMER0_IRQn);
 
-		for (i = 0; i < 1000000; i++);
-		GPIO_PinOutSet(LED0_port, LED0_pin);
-		//GPIO_PinOutSet(LED1_port, LED1_pin);
+  while (1)
+  {
+	  EMU_EnterEM2(false);
   }
 }
 
