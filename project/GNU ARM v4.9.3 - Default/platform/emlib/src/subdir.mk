970bcc2e61130c97583892dc65859ce1d590f45e
Ryan Mortenson <ryan.j.mortenson@gmail.com>
2018-01-27 21:44:59 -0700

    Add sleep based on EM levels

--- project/GNU ARM v4.9.3 - Default/platform/emlib/src/subdir.mk
+++ project/GNU ARM v4.9.3 - Default/platform/emlib/src/subdir.mk
@@ -11,6 +11,7 @@ C_SRCS += \
 ../platform/emlib/src/em_emu.c \
 ../platform/emlib/src/em_gpio.c \
 ../platform/emlib/src/em_i2c.c \
+../platform/emlib/src/em_letimer.c \
 ../platform/emlib/src/em_msc.c \
 ../platform/emlib/src/em_rmu.c \
 ../platform/emlib/src/em_rtcc.c \
@@ -25,6 +26,7 @@ OBJS += \
 ./platform/emlib/src/em_emu.o \
 ./platform/emlib/src/em_gpio.o \
 ./platform/emlib/src/em_i2c.o \
+./platform/emlib/src/em_letimer.o \
 ./platform/emlib/src/em_msc.o \
 ./platform/emlib/src/em_rmu.o \
 ./platform/emlib/src/em_rtcc.o \
@@ -39,6 +41,7 @@ C_DEPS += \
 ./platform/emlib/src/em_emu.d \
 ./platform/emlib/src/em_gpio.d \
 ./platform/emlib/src/em_i2c.d \
+./platform/emlib/src/em_letimer.d \
 ./platform/emlib/src/em_msc.d \
 ./platform/emlib/src/em_rmu.d \
 ./platform/emlib/src/em_rtcc.d \
@@ -93,6 +96,13 @@ platform/emlib/src/em_i2c.o: ../platform/emlib/src/em_i2c.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM C Compiler'
 	arm-none-eabi-gcc -g -gdwarf-2 -mcpu=cortex-m4 -mthumb -std=c99 '-D__HEAP_SIZE=0xD00' '-DHAL_CONFIG=1' '-D__STACK_SIZE=0x800' '-DEFR32BG1B232F256GM56=1' -I"/home/mortzula/Workspace/ecen_5823/project" -I"/home/mortzula/Workspace/ecen_5823/project/platform/radio/rail_lib/chip/efr32" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/gpiointerrupt/inc" -I"/home/mortzula/Workspace/ecen_5823/project/hardware/kit/common/halconfig" -I"/home/mortzula/Workspace/ecen_5823/project/hardware/kit/common/bsp" -I"/home/mortzula/Workspace/ecen_5823/project/platform/Device/SiliconLabs/EFR32BG1B/Include" -I"/home/mortzula/Workspace/ecen_5823/project/platform/CMSIS/Include" -I"/home/mortzula/Workspace/ecen_5823/project/hardware/kit/common/drivers" -I"/home/mortzula/Workspace/ecen_5823/project/platform/bootloader/api" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emlib/src" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emlib/inc" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/uartdrv/inc" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/tempdrv/src" -I"/home/mortzula/Workspace/ecen_5823/project/platform/halconfig/inc/hal-config" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/sleep/inc" -I"/home/mortzula/Workspace/ecen_5823/project/platform/radio/rail_lib/common" -I"/home/mortzula/Workspace/ecen_5823/project/protocol/bluetooth_2.7/ble_stack/inc/common" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/common/inc" -I"/home/mortzula/Workspace/ecen_5823/project/app/bluetooth_2.7/common/stack_bridge" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/tempdrv/inc" -I"/home/mortzula/Workspace/ecen_5823/project/protocol/bluetooth_2.7/ble_stack/inc/soc" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/sleep/src" -I"/home/mortzula/Workspace/ecen_5823/project/hardware/kit/EFR32BG1_BRD4302A/config" -I"/home/mortzula/Workspace/ecen_5823/project/platform/Device/SiliconLabs/EFR32BG1B/Source/GCC" -I"/home/mortzula/Workspace/ecen_5823/project/platform/Device/SiliconLabs/EFR32BG1B/Source" -I"/home/mortzula/Workspace/ecen_5823/project/platform/bootloader" -I"//home/mortzula/SimplicityStudio_v4/developer/toolchains/gnu_arm/4.9_2015q3//lib/gcc/arm-none-eabi/4.9.3/include" -I"/home/mortzula/Workspace/ecen_5823/project/src" -O0 -Wall -c -fmessage-length=0 -ffunction-sections -fdata-sections -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -MMD -MP -MF"platform/emlib/src/em_i2c.d" -MT"platform/emlib/src/em_i2c.o" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+platform/emlib/src/em_letimer.o: ../platform/emlib/src/em_letimer.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM C Compiler'
+	arm-none-eabi-gcc -g -gdwarf-2 -mcpu=cortex-m4 -mthumb -std=c99 '-D__HEAP_SIZE=0xD00' '-DHAL_CONFIG=1' '-D__STACK_SIZE=0x800' '-DEFR32BG1B232F256GM56=1' -I"/home/mortzula/Workspace/ecen_5823/project" -I"/home/mortzula/Workspace/ecen_5823/project/platform/radio/rail_lib/chip/efr32" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/gpiointerrupt/inc" -I"/home/mortzula/Workspace/ecen_5823/project/hardware/kit/common/halconfig" -I"/home/mortzula/Workspace/ecen_5823/project/hardware/kit/common/bsp" -I"/home/mortzula/Workspace/ecen_5823/project/platform/Device/SiliconLabs/EFR32BG1B/Include" -I"/home/mortzula/Workspace/ecen_5823/project/platform/CMSIS/Include" -I"/home/mortzula/Workspace/ecen_5823/project/hardware/kit/common/drivers" -I"/home/mortzula/Workspace/ecen_5823/project/platform/bootloader/api" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emlib/src" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emlib/inc" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/uartdrv/inc" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/tempdrv/src" -I"/home/mortzula/Workspace/ecen_5823/project/platform/halconfig/inc/hal-config" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/sleep/inc" -I"/home/mortzula/Workspace/ecen_5823/project/platform/radio/rail_lib/common" -I"/home/mortzula/Workspace/ecen_5823/project/protocol/bluetooth_2.7/ble_stack/inc/common" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/common/inc" -I"/home/mortzula/Workspace/ecen_5823/project/app/bluetooth_2.7/common/stack_bridge" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/tempdrv/inc" -I"/home/mortzula/Workspace/ecen_5823/project/protocol/bluetooth_2.7/ble_stack/inc/soc" -I"/home/mortzula/Workspace/ecen_5823/project/platform/emdrv/sleep/src" -I"/home/mortzula/Workspace/ecen_5823/project/hardware/kit/EFR32BG1_BRD4302A/config" -I"/home/mortzula/Workspace/ecen_5823/project/platform/Device/SiliconLabs/EFR32BG1B/Source/GCC" -I"/home/mortzula/Workspace/ecen_5823/project/platform/Device/SiliconLabs/EFR32BG1B/Source" -I"/home/mortzula/Workspace/ecen_5823/project/platform/bootloader" -I"//home/mortzula/SimplicityStudio_v4/developer/toolchains/gnu_arm/4.9_2015q3//lib/gcc/arm-none-eabi/4.9.3/include" -I"/home/mortzula/Workspace/ecen_5823/project/src" -O0 -Wall -c -fmessage-length=0 -ffunction-sections -fdata-sections -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -MMD -MP -MF"platform/emlib/src/em_letimer.d" -MT"platform/emlib/src/em_letimer.o" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- project/main.c
+++ project/main.c
@@ -113,6 +113,7 @@ int main(void)
 
   // Initialize device
   initMcu();
+
   // Initialize board
   initBoard();
 
