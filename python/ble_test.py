ea5413e4cc8d71301607d567c2c5af1b8451895e
Ryan Mortenson <ryan.j.mortenson@gmail.com>
2018-03-04 14:01:18 -0700

    Add thread to read data

--- python/ble_test.py
+++ python/ble_test.py
@@ -1,10 +1,22 @@
 import argparse
 import logging
 import sys
+import threading
 
 from bluepy import btle
 
 log = None
+NAME_DESCRIPTOR = "Complete Local Name"
+NAME = "Empty Example"
+
+def handle_device_connection(addr):
+    log.info("Handling device")
+    log.info("Attempting to connect to: {}".format(addr))
+    dev = btle.Peripheral(addr)
+
+    log.info("Here are a list of services")
+    for svc in dev.services:
+        log.info(svc)
 
 class ScanPrint(btle.DefaultDelegate):
 
@@ -12,12 +24,16 @@ class ScanPrint(btle.DefaultDelegate):
         btle.DefaultDelegate.__init__(self)
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
-        log.info(dev.getScanData())
+        scan_data = dev.getScanData()
+        for (sdid, desc, val) in scan_data:
+            if desc == NAME_DESCRIPTOR and val == NAME:
+                log.info("Found device {} reading values".format(dev.addr))
+                t = threading.Thread(target=handle_device_connection, args=(dev.addr, ))
+                t.start()
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("bssid", type=str)
     parser.add_argument("--verbosity", "-v", type=str, choices=["DEBUG", "INFO"],
          default="INFO", help="Level of verbosity")
     args = parser.parse_args()
@@ -27,15 +43,4 @@ if __name__ == "__main__":
     log = logging.getLogger("ble_test")
 
     scanner = btle.Scanner(0).withDelegate(ScanPrint())
-    devices = scanner.scan(5)
-
-    log.info(len(devices))
-
-    '''
-    log.info("Attempting to connect to: {}".format(args.bssid))
-    dev = btle.Peripheral(args.bssid)
-
-    log.info("Here are a list of services")
-    for svc in dev.services:
-        log.info(svc)
-    '''
+    devices = scanner.scan(25)
