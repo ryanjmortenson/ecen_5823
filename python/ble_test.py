62f230740c9ff38808db89006828bbc110b3af03
Ryan Mortenson <ryan.j.mortenson@gmail.com>
2018-03-05 21:59:13 +0000

    Clean up script

--- python/ble_test.py
+++ python/ble_test.py
@@ -20,46 +20,7 @@ CHAR_SOIL_MOISTURE_UUID = "83c77eb6-35af-4dd9-8851-87d0a92ea404"
 log = None
 client = None
 
-def handle_device_connection(addr):
-    log.info("Attempting to connect to: {}".format(addr))
-    try:
-        dev = btle.Peripheral(addr)
-    except Exception as e:
-        log.error("Could not connect to device: {}".format(addr))
-        return
-
-    try:
-        '''
-        log.info("Here are a list of services for {}".format(addr))
-        for svc in dev.services:
-            log.info(svc)
-            log.info("UUID: {}".format(svc.uuid))
-            for char in svc.getCharacteristics():
-                log.info("\t bssid {}: {}".format(addr, str(char)))
-                if char.supportsRead():
-                    val = char.read()
-                    if val.isalpha():
-                        log.info("\t\tbssid: {} value: {}".format(val))
-                    else:
-                        val = int(val[-1::-1].encode("hex"), 16)
-                        log.info("\t\tbssid: {} value (int): {}".format(addr, val))
-        '''
-        temp_char = dev.getCharacteristics(uuid=CHAR_TEMP_UUID)[0]
-        soil_char = dev.getCharacteristics(uuid=CHAR_SOIL_MOISTURE_UUID)[0]
-        temp = int(temp_char.read()[-1::-1].encode("hex"), 16)
-        soil = int(soil_char.read()[-1::-1].encode("hex"), 16)
-        client.celsiusWrite(addr + " TEMP", float(temp)/float(1000))
-        client.virtualWrite(addr + " MOISTURE", (float(soil)/float(4096) * 100), "Soil Moisture", "%")
-        log.info(temp)
-        log.info(soil)
-        log.info("Done listing items, disconnecting")
-        dev.disconnect()
-    except Exception as e:
-        log.error("Lost connection")
-        log.error(traceback.format_exc())
-
-class ScanPrint(btle.DefaultDelegate):
-
+class ConnectToDiscoveredDevice(btle.DefaultDelegate):
     def __init__(self):
         btle.DefaultDelegate.__init__(self)
 
@@ -67,15 +28,29 @@ class ScanPrint(btle.DefaultDelegate):
         scan_data = dev.getScanData()
         for (sdid, desc, val) in scan_data:
             if desc == NAME_DESCRIPTOR and val == NAME:
-                log.info("Found device {} reading values".format(dev.addr))
                 t = threading.Thread(target=handle_device_connection, args=(dev.addr, ))
                 t.start()
+
+def handle_device_connection(addr):
+    log.info("Attempting to connect to: {}".format(addr))
+    try:
+        dev = btle.Peripheral(addr)
+        temp_char = dev.getCharacteristics(uuid=CHAR_TEMP_UUID)[0]
+        soil_char = dev.getCharacteristics(uuid=CHAR_SOIL_MOISTURE_UUID)[0]
+        temp = int(temp_char.read()[-1::-1].encode("hex"), 16)
+        soil = int(soil_char.read()[-1::-1].encode("hex"), 16)
+        client.celsiusWrite(addr + " TEMP", float(temp)/float(1000))
+        client.virtualWrite(addr + " MOISTURE", (float(soil)/float(4096) * 100), "Soil Moisture", "%")
+        log.debug("TEMP: {}".format(temp))
+        log.debug("SOIL MOISTURE: {}".format(soil))
+        dev.disconnect()
+    except Exception as e:
+        log.error("Reading characteristics failed")
 
 def on_message(message):
       print("message received: " + str(message))
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--verbosity", "-v", type=str, choices=["DEBUG", "INFO"],
          default="INFO", help="Level of verbosity")
@@ -92,7 +67,7 @@ if __name__ == "__main__":
     while True:
         client.loop()
         log.info("Starting Scan...")
-        scanner = btle.Scanner(0).withDelegate(ScanPrint())
+        scanner = btle.Scanner(0).withDelegate(ConnectToDiscoveredDevice())
         devices = scanner.scan(30)
         log.info("Scan Complete, sleeping for 30 seconds")
         sleep(30)
